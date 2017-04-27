092868f27211bde46971dca296fee2cd90a46f33
Chumphae <singernice@hotmail.com>
2017-04-27 12:04:11 +0700

    JsonObject

--- app/src/main/java/cph/singersw/cphos/MainActivity.java
+++ app/src/main/java/cph/singersw/cphos/MainActivity.java
@@ -3,12 +3,16 @@ package cph.singersw.cphos;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-    public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView textView;
     private Button button;
         private String userString, passwordString;
@@ -64,12 +68,44 @@ import android.widget.TextView;
                 //Check Space
                 if (userString.equals("") || passwordString.equals("")) {
                     //Have Space
+                    Myalert myalert = new Myalert(MainActivity.this);
+                    myalert.myDialog("Have Speace","Plese Fill All Every Blank");
 
                 } else {
                     //No Spece
-                    Myalert myalert = new Myalert(MainActivity.this);
-                    myalert.myDialog("Have Speace","Plese Fill All Every Blank");
+                    checkUserAnPass();
                 }
+            }
+        }
+
+        private void checkUserAnPass() {
+            try {
+                String urlJSON="http://swiftcodingthai.com/cph/getDataCPHos.php";
+                boolean b = true;
+                String[] columnString = new String[]{"id","Name","User","Password"};
+                String[] loginString = new  String[columnString.length];
+
+
+                GetData getData = new GetData(MainActivity.this);
+                getData.execute(urlJSON);
+                String strJSON = getData.get();
+                Log.d("27AprilV1", "e checkUser ==>" +strJSON);
+
+                JSONArray jsonArray = new JSONArray(strJSON);
+
+                for (int i=0;i<jsonArray.length();i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    if (userString.equals(jsonObject.get(columnString[2]))) {
+                        b=false;
+                        for (int i1=0;i1<columnString.length;i1++) {
+                            loginString[i1] = jsonObject.getString(columnString[i1]);
+                            Log.d("27AprilV1","loginString(" + i1 + ") ==>" + loginString[i1]);
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                Log.d("27AprilV1", "e checkUser ==>" + e.toString());
             }
         }
 
